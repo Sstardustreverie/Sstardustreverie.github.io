da425c22e6943cc855cfbf177d570d303ad31845
Sstardustreverie <738515959@qq.com>
2025-12-16 23:37:14 +0800

    Site updated: 2025-12-16 23:37:14

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1765898026618";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1765899408708";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
